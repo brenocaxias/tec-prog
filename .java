5d56e1e21a67768171cd69726346908ba17eb1b9
brenocaxias <128320769+brenocaxias@users.noreply.github.com>
2023-03-22 17:16:36 -0300

    Create .java

---
+++ .java
@@ -0,0 +1,115 @@
+package lista1_2023;
+
+public class lista_01{ 
+	public static void main(String[] args) {
+
+		int a = 9;
+		int b = 11;
+    
+		// Question 01
+		if (a > 10)
+			System.out.println("A > 10");
+		if ((a + b) == 20)
+			System.out.println("A + B == 20");
+		else
+			System.out.println("número não válido");
+
+		// Question 02
+		if (a < 10)
+			System.out.println("A < 10");
+		else if ((a + b == 20))
+			System.out.println("A + B == 20");
+		else
+			System.out.println("número não válido");
+
+		// Question 03
+		if (a == 10)
+			System.out.println("A == 10");
+		if ((a + b) == 20)
+			System.out.println("A + B == 20");
+		if (b == 10)
+			System.out.println("B == 10");
+
+		// Question 04
+		if (a > 10 || (a + b) == 20)
+			System.out.println("número válido");
+		else if (a == b)
+			System.out.println("A é igual B; A e B são diferentes de 10; A é menor que 10");
+		else
+			System.out.println("número não válido");
+
+		// Question 05
+		if (a > 10 || (a + b) == 20)
+			System.out.println("número válido");
+		else
+			System.out.println("número não válido");
+
+		// Question 06
+		if (a > 10)
+			System.out.println("A > 10");
+		else
+			System.out.println("A <= 10");
+		if ((a + b) == 20)
+			System.out.println("A + b == 20");
+		else
+			System.out.println("A + B != 20");
+
+		// Question 07
+		if (a > 10 || (a + b) == 20)
+			System.out.println("números válidos");
+		else
+			System.out.println("números não válidos");
+
+		// Question 08
+		if (a > 10)
+			System.out.println("A > 10");
+		else if ((a + b) == 20)
+			System.out.println("A + b == 20");
+		else
+			System.out.println("número não válido");
+
+		// Question 09
+		if (a > 10)
+			if ((a + b) == 20)
+				System.out.println("A + B == 20");
+			else
+				System.out.println("número não válido");
+
+		// Question 10
+		if (a > 10)
+			System.out.println();
+		else
+			System.out.println("número menor que 10");
+		if ((a + b) == 20)
+			System.out.println();
+		else
+			System.out.println("número diferente de 20");
+
+		// Question 11
+		if (a > 10)
+			System.out.println();
+
+		else if ((a + b) == 20)
+			System.out.println("A + B == 20");
+		else
+			System.out.println("número não válido");
+
+		// Question 12
+		if (a > 10)
+			System.out.println("A > 10");
+		else if ((a + b) == 20)
+			System.out.println("A + B == 20");
+		else
+			System.out.println("números não válidos");
+		System.out.println("Bem-vindos à disciplina de técnicas de programação");
+
+		// Question 13
+		if (a > 10)
+			System.out.println("A > 10");
+		if ((a + b) == 20)
+			System.out.println("A + B == 20");
+		else
+			System.out.println("números não válidos");
+
+	}
+}
